9fe26c8e9f752094640b78b1aaad29d50bae1098
icebear493 <min-yue493@protonmail.com>
2020-05-04 21:25:43 -0400

    queation 0001 two sum solution

--- src/q0001twoSum.java
+++ src/q0001twoSum.java
@@ -1,2 +1,20 @@
+//@author minyue
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
 public class q0001twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] output = new int[2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    output[0]=i;
+                    output[1]=j;
+                    return output;
+                }
+            }
+        }
+        return output;
+        //throw new IllegalArgumentException("No two sum solution");
+    }
 }
